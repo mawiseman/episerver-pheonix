c2f0623347cd37b95dbb9ed410055bed612a266f
Mark Wiseman <mark.a.wiseman@avanade.com>
2020-12-16 16:13:23 +1100

    updated epi version and added default ccounts

---
+++ src/Phoenix/Foundation/DebugUsers/Business/Initialization/CreateAdminUserForMembershipInitiialization.cs
@@ -0,0 +1,47 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Logging.Compatibility;
+using System.Configuration.Provider;
+using System.Web.Security;
+
+namespace DebugUsers.Business.Initialization
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class CreateAdminUserForMembershipInitiialization : IInitializableModule
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(CreateAdminUserForMembershipInitiialization));
+
+        public void Initialize(InitializationEngine context)
+        {
+#if DEBUG
+            AddUser("Admin", "Grover68!", new[] { "WebAdmins", "WebEditors" });
+#endif
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+
+        private void AddUser(string username, string password, string[] roles)
+        {
+            var mu = Membership.GetUser(username);
+            
+            if (mu == null)
+            {
+                Membership.CreateUser(username, password, username + "@site.com");
+            }
+
+            var existingRoles = Roles.GetRolesForUser(username);
+            if(existingRoles.Length > 0)
+            {
+                Roles.RemoveUserFromRoles(username, existingRoles);
+            }
+            
+            if(roles.Length > 0)
+            {
+                Roles.AddUserToRoles(username, roles);
+            }
+        }
+    }
+}
---
+++ src/Phoenix/Foundation/DebugUsers/ReadMe.md
@@ -0,0 +1,24 @@
+﻿# DebugUsers
+
+## Introduction
+
+When the application is running in Debug mode this feature will ensure that we have the following accounts avalaible
+
+| User | Password | Roles |
+|-|-|-|
+|Admin|Grover68!|XX|
+
+
+## Usage
+
+For this to work we need to ensure that the membership provider is set to `SqlServerMembershipProvider`
+
+This can be done by updating te `web.config` file
+
+```
+<configuration>
+  <system.web>
+    <membership defaultProvider="SqlServerMembershipProvider" ... />
+    <roleManager defaultProvider="SqlServerRoleProvider" ... />
+</configuration>
+```
